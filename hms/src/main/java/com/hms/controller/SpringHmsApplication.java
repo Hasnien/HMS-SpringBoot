6cf9e6e6ddd84d09a7e4c5a25a885b4f27191e7f
Hasnien Farazee Shahed <hasnienfarazeeshahed@gmail.com>
2024-09-11 15:05:10 +0600

    project initiated

---
+++ hms/src/main/java/com/hms/controller/SpringHmsApplication.java
@@ -0,0 +1,20 @@
+package com.hms.controller;
+
+import com.hms.model.PathologicalTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+public class SpringHmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHmsApplication.class, args);
+		System.out.println("Hello Spring");
+//		List<PathologicalTest> Tests = new ArrayList<>();
+
+	}
+
+}

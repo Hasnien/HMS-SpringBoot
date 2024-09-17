c3413eab436fb760dc42d02aea06fa18d96903a7
Hasnien Farazee Shahed <hasnienfarazeeshahed@gmail.com>
2024-09-17 18:59:36 +0600

    new update

--- hms/src/main/java/com/hms/controller/UserController.java
+++ hms/src/main/java/com/hms/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.hms.controller;
+import com.hms.model.User;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    public User saveUser(@RequestBody User user){
+        return user;
+    }
+
 
 }
